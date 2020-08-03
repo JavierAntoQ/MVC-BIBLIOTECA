42daad492d8c415218f0e6c9dfadc9643110eddd
JavierQuispe <1813011329@UNTELS.EDU.PE>
2020-08-03 12:03:44 -0500

    MVC BIBLIOTECA MODELO.JAVA TERMINADO

--- src/java/Modelo/Biblioteca.java
+++ src/java/Modelo/Biblioteca.java
@@ -10,5 +10,70 @@ package Modelo;
  * @author USER
  */
 public class Biblioteca {
+    private int idlibro;
+    private String titulo;  
+    private int cantidad;
+    private int anio;
+    private double precio;
+    private String estado;
+
+    public Biblioteca(int idlibro, String titulo, int cantidad, int anio, double precio, String estado) {
+        this.idlibro = idlibro;
+        this.titulo = titulo;
+        this.cantidad = cantidad;
+        this.anio = anio;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
     
 }
